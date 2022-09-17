6857cbd5c43fc57e99209ce24b4a4ba42f1bf2b3
Aaron Liu <aaron155658@gmail.com>
2022-09-17 18:54:44 -0400

    structure folders

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+const path = require("path");
 const express = require("express");
 const routes = require("./routes");
+const exphbs = require("express-handlebars");
+
 const sequelize = require("./config/config");
+
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 //attach middleware to server
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./controllers/"));
 
 // turn on routes
 app.use(routes);
