ae62d0b189b0fe02e1f57b3636e6cf58e7c2e294
Aaron Liu <aaron155658@gmail.com>
2022-09-20 19:34:47 -0400

    MVC-2

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,9 @@
-//create main router for server
 const router = require("express").Router();
 
-const apiRoutes = require("./api");
+const apiRoutes = require("./api/");
+const homeRoutes = require("./home-routes.js");
 
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
 
 module.exports = router;
