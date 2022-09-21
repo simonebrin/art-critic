cd947a5dca5b29c5ade1fa2ae609eb4ff1d61da1
Emily <em.smolka@gmail.com>
2022-09-21 19:03:12 -0400

    created separate page for user comments and price

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,33 @@ const router = require("express").Router();
 const sequelize = require("../config/config");
 const { Art, Comment, Eval, User } = require("../models");
 
+router.get("/art/:id", async (req, res) => {
+  try {
+    // const artData = await Art.findAll({
+    //   include: [Comment, {
+    //     model: User
+    //   }], [Eval]
+    // });
+    const artData = await Art.findByPk(req.params.id, {
+      include: [
+        { model: Comment, include: User },
+        {
+          model: Eval,
+          include: User,
+        },
+      ],
+    });
+    //const art = artData.map((piece) => piece.get({ plain: true }));
+    const art = artData.get({ plain: true });
+    //console.log(art);
+    //console.log(art[0].evals);
+    res.render("art", { ...art });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
 // get all posts for homepage
 /*router.get("/", (req, res) => {
   console.log("======================");
@@ -26,15 +53,19 @@ router.get("/", async (req, res) => {
     // });
     const artData = await Art.findAll({
       include: [
-        {model: Comment,
-        include: User,}
+        { model: Comment, include: User },
+        {
+          model: Eval,
+          include: User,
+        },
       ],
     });
     const art = artData.map((piece) => piece.get({ plain: true }));
     console.log(art);
-    console.log(art[0].comments)
+    console.log(art[0].evals);
     res.render("homepage", { art });
   } catch (error) {
+    console.error(error);
     res.status(500).json(error);
   }
 });
