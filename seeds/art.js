e7d6fb2ecc54fce5f89ec5554edfc43fc3ea9422
Simone Brin <simonebrin@gmail.com>
2022-09-22 10:57:21 -0400

    updated layouts, seeds etc.

--- seeds/art.js
+++ seeds/art.js
@@ -3,23 +3,23 @@ const { Art } = require("../models/index");
 
 async function seedArt() {
   await Art.create({
-    name: "piece1",
+    name: "Ed Sun",
     image:
       "https://xavierhufkens.imgix.net/artworks/NEEL-033-1971.jpg?auto=format&crop=focalpoint&domain=xavierhufkens.imgix.net&fit=clip&w=150&h=1976.833976834&q=82&fm=pjpg&fp-x=0.5&fp-y=0.5&ixlib=php-2.1.1",
   });
 
   await Art.create({
-    name: "piece2",
+    name: "Hartley on the Motorcycle",
     image:
       "https://xavierhufkens.imgix.net/artworks/NEEL-046-1966.jpg?auto=format&crop=focalpoint&domain=xavierhufkens.imgix.net&fit=clip&w=150&h=2416.9944925256&q=82&fm=pjpg&fp-x=0.5&fp-y=0.5&ixlib=php-2.1.1",
   });
   await Art.create({
-    name: "piece3",
+    name: "Nancy and the Twins (5 Months)",
     image:
       "https://xavierhufkens.imgix.net/artworks/NEEL-029-1971.jpg?auto=format&crop=focalpoint&domain=xavierhufkens.imgix.net&fit=clip&w=150&h=1149.90234375&q=82&fm=pjpg&fp-x=0.5&fp-y=0.5&ixlib=php-2.1.1",
   });
   await Art.create({
-    name: "piece4",
+    name: "Carolyn Robinson",
     image:
       "https://xavierhufkens.imgix.net/artworks/NEEL-087-1974_rf_2021-08-14-130450.jpg?auto=format&crop=focalpoint&domain=xavierhufkens.imgix.net&fit=clip&w=150&h=1725.4509018036&q=82&fm=pjpg&fp-x=0.5&fp-y=0.5&ixlib=php-2.1.1",
   });
--- seeds/comment.js
+++ seeds/comment.js
@@ -9,24 +9,24 @@ async function seedComment() {
   });
   await Comment.create({
     comment_text: "Wow I love it!",
-    art_id: 1,
-    user_id: 1,
-  });
-  await Comment.create({
-    comment_text: "piece2",
     art_id: 2,
     user_id: 1,
   });
   await Comment.create({
-    comment_text: "piece3",
+    comment_text: "piece2",
     art_id: 3,
     user_id: 2,
   });
   await Comment.create({
-    comment_text: "piece4",
+    comment_text: "piece3",
     art_id: 4,
     user_id: 1,
   });
+  // await Comment.create({
+  //   comment_text: "piece4",
+  //   art_id: 4,
+  //   user_id: 1,
+  // });
   console.log("=======comments Seeded=======");
 }
 
