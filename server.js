83a1ad444978e7b10defc08fdacb7f63d8722725
Simone Brin <simonebrin@gmail.com>
2022-09-30 09:55:48 -0400

    more tests

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/config");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-require("./seeds");
+// require("./seeds");
 
 const sess = {
   secret: "Super secret secret",
